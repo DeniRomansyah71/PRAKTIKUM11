d5bd984a623e38fc3f872a7c904aed5a08f6ea5f
DeniRomansyah71 <dennyzromansyah71@gmail.com>
2019-12-08 20:06:36 +0700

    deni

---
+++ modul11/src/percobaan4/Relation.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package percobaan4;
+
+/**
+ *
+ * @author deni romansyah
+ */
+public interface Relation {
+    public boolean isGreater( Object a, Object b);
+    public boolean isLess( Object a, Object b);
+    public boolean isEqual( Object a, Object b);    
+}
